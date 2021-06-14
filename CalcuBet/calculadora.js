e08fc7a211ac3d73063fbb8b6ebdfbbe34a65e7a
BeatrizLoor11 <76933683+BeatrizLoor11@users.noreply.github.com>
2021-06-13 22:15:46 -0500

    Add files via upload

---
+++ CalcuBet/calculadora.js
@@ -0,0 +1,38 @@
+"use strict";
+(() => {
+
+    const suma = (a, b) => a + b;
+    
+    console.log(suma(2,8));
+
+
+})();
+var a = 1;
+var b = 1;
+const c=81;
+
+function calcular(){
+    suma(a,b)
+    resta(a,b)
+    division(a,b)
+    RaizCuadrada(c)
+}
+
+function suma(numeroA, numeroB){
+    console.log(numeroA + numeroB);
+}
+
+function resta(numeroA, numeroB){
+    console.log(numeroA - numeroB);
+}
+
+function division(numeroA, numeroB){
+    console.log(numeroA / numeroB);
+}
+
+function RaizCuadrada(constc){
+    const raizCuadrada = Math.sqrt(constc);
+    console.log(raizCuadrada);
+}
+
+calcular();
